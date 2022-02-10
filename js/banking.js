1a1aeb4c602ba27da6845339ab75ecb482360152
Your Name <you@example.com>
2022-02-10 12:48:16 -0800

    add event listener to deposit

--- js/banking.js
+++ js/banking.js
@@ -1,14 +1,22 @@
+// deposit handle
 const depositBtn = document.getElementById("deposit-btn");
 depositBtn.addEventListener("click", function () {
   // new amount
   const depositAmount = document.getElementById("deposit-amount");
+  const depositValue = parseFloat(depositAmount.value);
   //   previous amount
   const depositUpdate = document.getElementById("deposit-update");
   //   add new value with previous
-  const totalDeposit =
-    parseFloat(depositAmount.value) + parseFloat(depositUpdate.innerText);
+  const totalDeposit = depositValue + parseFloat(depositUpdate.innerText);
   //   set total value as innertext
   depositUpdate.innerText = totalDeposit;
+  depositAmount.value = "";
+
+  //   update total balance
+  const totalBalance = document.getElementById("total-balance");
+  const newBalance = parseFloat(totalBalance.innerText) + depositValue;
+  totalBalance.innerText = newBalance;
+
   //   clear text field
   depositAmount.value = "";
 });
