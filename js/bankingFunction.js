c587ca2b77b404ea244e11448ce206a763c79dca
Your Name <you@example.com>
2022-02-11 05:51:16 -0800

    add error handler

--- js/bankingFunction.js
+++ js/bankingFunction.js
@@ -9,7 +9,7 @@ function previousAmount(pValue) {
   const ptextValue = parseFloat(ptextId.innerText);
   return ptextValue;
 }
-function totalBalanceAmount(isAdd, value2) {
+function totalBalance(isAdd, value2) {
   const tid = document.getElementById("total-balance");
   const tValue = parseFloat(tid.innerText);
   if (isAdd == true) {
@@ -18,23 +18,35 @@ function totalBalanceAmount(isAdd, value2) {
     tid.innerText = tValue - value2;
   }
 }
+function getCurrentBalance() {
+  return parseFloat(document.getElementById("total-balance").innerText);
+}
 // deposit
 const depositBtn = document.getElementById("deposit-btn");
 depositBtn.addEventListener("click", function () {
-  const newDepositAmount = newAmount("deposit-amount");
-  const previousDepositAmount = previousAmount("deposit-update");
-  //   update deposit
-  document.getElementById("deposit-update").innerText =
-    newDepositAmount + previousDepositAmount;
-  totalBalanceAmount(true, newDepositAmount);
+  const newDeposit = newAmount("deposit-amount");
+  if (newDeposit > 0) {
+    const previousDeposit = previousAmount("deposit-update");
+    //   update deposit
+    document.getElementById("deposit-update").innerText =
+      newDeposit + previousDeposit;
+    totalBalance(true, newDeposit);
+  } else {
+    alert("Invalid request");
+  }
 });
 // withdraw
 const withdrawBtn = document.getElementById("withdraw-btn");
 withdrawBtn.addEventListener("click", function () {
-  const newWithdrawAmount = newAmount("withdraw-amount");
-  const pWithdraw = previousAmount("withdraw-update");
-  //   update withdraw
-  document.getElementById("withdraw-update").innerText =
-    newWithdrawAmount + pWithdraw;
-  totalBalanceAmount(false, newWithdrawAmount);
+  const newWithdraw = newAmount("withdraw-amount");
+
+  if (newWithdraw > 0 && getCurrentBalance() >= newWithdraw) {
+    const previousWithdraw = previousAmount("withdraw-update");
+    //   update withdraw
+    document.getElementById("withdraw-update").innerText =
+      newWithdraw + previousWithdraw;
+    totalBalance(false, newWithdraw);
+  } else {
+    alert("Invalid request");
+  }
 });
